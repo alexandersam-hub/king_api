83f0dc1327cf40168f09f22bc36817961c2f4fcc
alexandersam-hub <alexandersamedov@gmail.com>
2021-05-17 08:27:57 +0300

    first commit

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
+
+echo 1+1;
